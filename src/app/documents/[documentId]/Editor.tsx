39027bde09ff77998e3200ec210bb6db8f865126
Muhammad Rafai <rafaimhd123@gmail.com>
2024-12-21 20:45:44 +0700

    feat: - Create and style Editor.tsx component - Create setup for Editor TipTap and install extensions (include : StarterKit, TaskList, Table, Image, DropCursor, ImageResizeand heading

---
+++ src/app/documents/[documentId]/Editor.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+// import Heading from "@tiptap/extension-heading";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Text from "@tiptap/extension-text";
+import Image from '@tiptap/extension-image'
+import Dropcursor from '@tiptap/extension-dropcursor'
+import ImageResize from 'tiptap-extension-resize-image';
+
+const Editor = () => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        style: "padding-right: 56px; padding-left: 56px;",
+        class: "tiptap focus:outline-none print:bg-white print:border-0 border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      Dropcursor,
+      ImageResize,
+      // Heading.configure({
+      //   levels: [1, 2, 3],
+      // }),
+    ],
+    content: `
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th colspan="3">Description</th>
+          </tr>
+          <tr>
+            <td>Cyndi Lauper</td>
+            <td>Singer</td>
+            <td>Songwriter</td>
+            <td>Actress</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>This is a basic example of implementing images. Drag to re-order.</p>
+      <img src="https://placehold.co/800x400" />
+    `,
+  });
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,12 @@
+import Editor from "./Editor"
+
+
+const DocumentIdPage = () => {
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />
+    </div>
+  )
+}
+
+export default DocumentIdPage
